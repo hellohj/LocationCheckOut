7d7346a4a8178b6b33aae608e1ff5f4c67975ed3
Hye Jung Choi <hellohj@gmail.com>
2016-02-25 17:38:40 -0800

    revert SlidingUpPanelLayout library to original version and add a few listeners for map gestures

--- app/src/main/java/com/hjchoi/locationcheckout/MapsActivity.java
+++ app/src/main/java/com/hjchoi/locationcheckout/MapsActivity.java
@@ -8,12 +8,12 @@ import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.client.ChildEventListener;
@@ -38,13 +38,13 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -55,27 +55,19 @@ public class MapsActivity extends FragmentActivity implements
         OnMapReadyCallback,
         ChildEventListener,
         LocationListener,
-        SlidingUpPanelLayout.PanelSlideListener,
-        HeaderAdapter.ItemClickListener {
+        SlidingUpPanelLayout.PanelSlideListener {
 
     private static final String LOG_TAG = MapsActivity.class.getSimpleName();
 
     @Bind(R.id.checkout_button) Button mCheckoutButton;
     private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
     private GoogleApiClient mGoogleApiClient;
     private LatLngBounds.Builder mBounds = new LatLngBounds.Builder();
     private static final int REQUEST_PLACE_PICKER = 1;
     private Firebase mFirebase;
-
     // related to SlidingUpPanel
-    @Bind(android.R.id.list) LockableRecyclerView mListView;
-    @Bind(R.id.sliding_layout) SlidingUpPanelLayout mSlidingUpPanelLayout;
-    @Bind(R.id.transparentView) View mTransparentView;
-    private HeaderAdapter mHeaderAdapter;
-
-//    @Bind(R.id.sliding_layout) SlidingUpPanelLayout mLayout;
-//    @Bind(R.id.name) TextView mName;
-//    @Bind(R.id.list) ListView mListView;
+    @Bind(R.id.sliding_layout) SlidingUpPanelLayout mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,88 +77,41 @@ public class MapsActivity extends FragmentActivity implements
         setUpGoogleMap();
         setUpFireBase();
         setUpSlidingUpPanel();
-//        setUpSlidingUpPanel2();
     }
 
-//    private void setUpSlidingUpPanel2() {
-//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    private void setUpSlidingUpPanel() {
+
+        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
+        mLayout.addPanelSlideListener(this);
+        mLayout.setFadeOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+            }
+        });
+
+
+        TextView t = (TextView) findViewById(R.id.name);
+        t.setText(Html.fromHtml("hello"));
+        Button f = (Button) findViewById(R.id.follow);
+        f.setText(Html.fromHtml("follow"));
+//        f.setMovementMethod(LinkMovementMethod.getInstance());
+//        f.setOnClickListener(new View.OnClickListener() {
 //            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Toast.makeText(MapsActivity.this, "onItemClick", Toast.LENGTH_SHORT).show();
+//            public void onClick(View v) {
+//                Intent i = new Intent(Intent.ACTION_VIEW);
+//                i.setData(Uri.parse("http://www.twitter.com/umanoapp"));
+//                startActivity(i);
 //            }
 //        });
-//
-//        List<String> your_array_list = Arrays.asList(
-//                "item 1"
-//        );
-//
-//        // This is the array adapter, it takes the context of the activity as a
-//        // first parameter, the type of list view as a second parameter and your
-//        // array as a third parameter.
-//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
-//                this,
-//                android.R.layout.simple_list_item_1,
-//                your_array_list );
-//
-//        mListView.setAdapter(arrayAdapter);
-//
-//        mLayout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
-//        mLayout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
-//            @Override
-//            public void onPanelSlide(View panel, float slideOffset) {
-//                Log.i(LOG_TAG, "onPanelSlide, offset " + slideOffset);
-//            }
-//
-//            @Override
-//            public void onPanelExpanded(View panel) {
-//                Log.i(LOG_TAG, "onPanelExpanded");
-//
-//            }
-//
-//            @Override
-//            public void onPanelCollapsed(View panel) {
-//                Log.i(LOG_TAG, "onPanelCollapsed");
-//
-//            }
-//
-//            @Override
-//            public void onPanelAnchored(View panel) {
-//                Log.i(LOG_TAG, "onPanelAnchored");
-//            }
-//
-//            @Override
-//            public void onPanelHidden(View panel) {
-//                Log.i(LOG_TAG, "onPanelHidden");
-//            }
-//        });
-//        mLayout.setFadeOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
-//            }
-//        });
-//
-//        mName.setText("panel content");
-////        t.setText(Html.fromHtml(getString(R.string.hello)));
-////        Button f = (Button) findViewById(R.id.follow);
-////        f.setText(Html.fromHtml(getString(R.string.follow)));
-////        f.setMovementMethod(LinkMovementMethod.getInstance());
-////        f.setOnClickListener(new View.OnClickListener() {
-////            @Override
-////            public void onClick(View v) {
-////                Intent i = new Intent(Intent.ACTION_VIEW);
-////                i.setData(Uri.parse("http://www.twitter.com/umanoapp"));
-////                startActivity(i);
-////            }
-////        });
-//    }
+    }
 
     private void setUpGoogleMap() {
         Log.d(LOG_TAG, "setUpGoogleMap");
         // Set up Google Maps
-        SupportMapFragment mapFragment = (SupportMapFragment)
+        mMapFragment = (SupportMapFragment)
                 getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mMapFragment.getMapAsync(this);
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
                     .addApi(Places.GEO_DATA_API)
@@ -180,36 +125,6 @@ public class MapsActivity extends FragmentActivity implements
         Firebase.setAndroidContext(this);
         mFirebase = new Firebase(BuildConfig.FIREBASE_URL);
         mFirebase.child(BuildConfig.FIREBASE_ROOT_NODE).addChildEventListener(this);
-    }
-
-    private void setUpSlidingUpPanel() {
-        Log.d(LOG_TAG, "setUpSlidingUpPanel");
-        // Set up slidingUpPanel
-        mListView.setOverScrollMode(ListView.OVER_SCROLL_NEVER);
-        mSlidingUpPanelLayout.setEnableDragViewTouchEvents(true);
-        int mapHeight = getResources().getDimensionPixelSize(R.dimen.map_height);
-        mSlidingUpPanelLayout.setPanelHeight(mapHeight); // you can use different height here
-        mSlidingUpPanelLayout.setScrollableView(mListView, mapHeight);
-        mSlidingUpPanelLayout.setPanelSlideListener(this);
-//        collapseMap();
-        expandMap();
-        mSlidingUpPanelLayout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                mSlidingUpPanelLayout.getViewTreeObserver().removeGlobalOnLayoutListener(this);
-                mSlidingUpPanelLayout.onPanelDragged(0);
-            }
-        });
-
-        ArrayList<String> testData = new ArrayList<>();
-        testData.add("item 1");
-
-        mHeaderAdapter = new HeaderAdapter(this, testData, this);
-        mListView.setItemAnimator(null);
-        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        mListView.setLayoutManager(layoutManager);
-        mListView.setAdapter(mHeaderAdapter);
     }
 
     protected void onStart() {
@@ -315,7 +230,23 @@ public class MapsActivity extends FragmentActivity implements
             public boolean onMarkerClick(Marker marker) {
                 // Handle marker click here
                 Log.d(LOG_TAG, "marker clicked: " + marker.getPosition());
-                return false;
+//                LatLng points = new LatLng(marker.getPosition().latitude, marker.getPosition().longitude);
+                if (mLayout != null) {
+                    mLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+                    TextView tvName = (TextView) findViewById(R.id.name);
+                    tvName.setText(marker.getPosition().toString());
+                } else {
+                    mLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
+                }
+                return true;
+            }
+        });
+        mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                Log.d(LOG_TAG, "map onCameraChange: let's set panel state hidden");
+                // when a user moves on, disable sliding panel so that a user can click a marker
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
             }
         });
     }
@@ -360,77 +291,53 @@ public class MapsActivity extends FragmentActivity implements
 
     @Override
     public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
+        Log.d(LOG_TAG, "onChildChanged");
     }
 
     @Override
     public void onChildRemoved(DataSnapshot dataSnapshot) {
-
+        Log.d(LOG_TAG, "onChildChanged");
     }
 
     @Override
     public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
+        Log.d(LOG_TAG, "onChildRemoved");
     }
 
     @Override
     public void onCancelled(FirebaseError firebaseError) {
-
-    }
-
-    @Override
-    public void onItemClicked(int position) {
-        Log.d(LOG_TAG, "item clicked: " + position);
-        mSlidingUpPanelLayout.collapsePane();
-    }
-
-    private void collapseMap() {
-        Log.d(LOG_TAG, "collapseMap");
-        if (mHeaderAdapter != null) {
-            Log.d(LOG_TAG, "header adapter is not null: " + mHeaderAdapter.getItemCount());
-            mHeaderAdapter.showSpace();
-        }
-        mTransparentView.setVisibility(View.GONE);
-        if (mMap != null) {
-            Log.d(LOG_TAG, "mMap is not null at this moment");
-            //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mLocation, 11f), 1000, null);
-        }
-        //mListView.setScrollingEnabled(true);
-    }
-
-    private void expandMap() {
-        Log.d(LOG_TAG, "expandMap");
-        if (mHeaderAdapter != null) {
-            Log.d(LOG_TAG, "header adapter is not null: " + mHeaderAdapter.getItemCount());
-            mHeaderAdapter.hideSpace();
-        }
-        mTransparentView.setVisibility(View.INVISIBLE);
-        if (mMap != null) {
-            Log.d(LOG_TAG, "mMap is not null at this moment");
-            //mMap.animateCamera(CameraUpdateFactory.zoomTo(14f), 1000, null);
-        }
-        //mListView.setScrollingEnabled(false);
+        Log.d(LOG_TAG, "onChildRemoved");
     }
 
     @Override
     public void onPanelSlide(View panel, float slideOffset) {
-        Log.d(LOG_TAG, "onPanelSlide");
-    }
-
-    @Override
-    public void onPanelCollapsed(View panel) {
-        Log.d(LOG_TAG, "onPanelCollapsed - expandMap");
-        expandMap();
+        Log.d(LOG_TAG, "onPanelSlide, offset " + slideOffset);
+        mMap.getUiSettings().setAllGesturesEnabled(true);
     }
 
     @Override
     public void onPanelExpanded(View panel) {
-        Log.d(LOG_TAG, "onPanelExpanded - collapseMap");
-        collapseMap();
+        Log.d(LOG_TAG, "onPanelExpanded");
+        mMap.getUiSettings().setAllGesturesEnabled(false);
+    }
+
+    @Override
+    public void onPanelCollapsed(View panel) {
+
+        Log.d(LOG_TAG, "onPanelCollapsed");
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+        //mLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
     }
 
     @Override
     public void onPanelAnchored(View panel) {
         Log.d(LOG_TAG, "onPanelAnchored");
     }
+
+    @Override
+    public void onPanelHidden(View panel) {
+        Log.d(LOG_TAG, "onPanelHidden");
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+    }
+
 }
